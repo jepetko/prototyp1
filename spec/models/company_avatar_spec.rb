8c7f6da73c0cadc60f114d3fadba370529b576e9
katarina <golbang.k@gmail.com>
2013-07-15 17:41:06 +0200

    model tests for customer + company_avatar

--- spec/models/company_avatar_spec.rb
+++ spec/models/company_avatar_spec.rb
@@ -1,5 +1,44 @@
 require 'spec_helper'
+require 'paperclip'
 
 describe CompanyAvatar do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+   before(:each) do
+     @customer = FactoryGirl.create(:customer)
+
+     att = Paperclip::Attachment.new(:avatar, @customer, {
+         default_url: '/images/:style/missing.png',
+         path: ":rails_root/public/system/:attachment/:id/:style/:filename",
+         url: "/system/:attachment/:id/:style/:filename"
+     });
+     @attr = {
+        avatar: att
+     }
+
+   end
+
+  it "responds to methods" do
+    avatar = CompanyAvatar.new(@attr)
+    avatar.should respond_to(:customer)
+  end
+
+  it "is valid when valid image is uploaded" do
+    avatar = CompanyAvatar.new(@attr)
+    avatar.should be_valid
+
+    expect {
+      avatar.save!
+    }.to change(CompanyAvatar,:count).by(1)
+  end
+
+  it "is valid when image is nil too" do
+    avatar = CompanyAvatar.new(@attr.merge(:avatar => nil))
+    avatar.should be_valid
+
+    expect {
+      avatar.save!
+    }.to change(CompanyAvatar,:count).by(1)
+  end
+
+
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,49 @@
 require 'spec_helper'
 
 describe Customer do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:each) do
+    @attr = {
+        city: 'Vienna',
+        country: 'AUT',
+        name: 'Company Limited',
+        street: 'Funny Street 1',
+        zip: '1010'
+    }
+  end
+
+  it "responds to methods" do
+    customer = Customer.create!(@attr)
+
+    customer.should respond_to(:company_avatar)
+    customer.should respond_to(:contacts)
+  end
+
+  it "stores valid record" do
+    expect {
+      customer = Customer.new(@attr)
+      customer.should be_valid
+      customer.save
+    }.to change(Customer,:count).by(1)
+  end
+
+  it "doesn't store invalid record (missing city)" do
+    customer = Customer.new(@attr.merge(:city => nil))
+    customer.should_not be_valid
+
+    expect {
+      customer.save!
+    }.should raise_error ActiveRecord::RecordInvalid
+  end
+
+  it "doesn't store invalid record (duplicate name)" do
+    orig_customer = Customer.create!(@attr)
+    customer = Customer.new(@attr)
+    customer.should_not be_valid
+
+    expect {
+      customer.save!
+    }.should raise_error ActiveRecord::RecordInvalid
+  end
+
 end
