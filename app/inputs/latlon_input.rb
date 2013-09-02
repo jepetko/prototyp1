be90ee9afae4be309592c6d74efebded679ba80f
katarina <golbang.k@gmail.com>
2013-09-02 14:52:09 +0200

    custom input for latlon; tailoring the form to accept new attribute and to render it correctly

--- app/inputs/latlon_input.rb
+++ app/inputs/latlon_input.rb
@@ -1,5 +1,5 @@
 class LatlonInput < SimpleForm::Inputs::Base
   def input
-    "POINT(#{@builder.text_field(attribute_name, input_html_options)})".html_safe
+    @builder.text_field(attribute_name, input_html_options.merge(:value => @builder.object.latlon.to_s)).html_safe
   end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,7 +3,7 @@ class Customer < ActiveRecord::Base
   set_rgeo_factory_for_column(:latlon,
                               RGeo::Geographic.spherical_factory(:srid => 4326))
 
-  attr_accessible :city, :country, :name, :street, :zip, :company_avatar
+  attr_accessible :city, :country, :name, :street, :zip, :latlon, :company_avatar
 
   validates :name, :presence => true, :uniqueness => true
   validates :street, :presence => true
