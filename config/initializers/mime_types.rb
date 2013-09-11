5ea606de0d168b876f3009ac0193b266c1d51cd7
katarina <golbang.k@gmail.com>
2013-09-11 19:04:15 +0200

    must be aliased otherwise normal json requests wont work and RoR will demand geojson handling for every and each controller

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -4,5 +4,5 @@
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
 
-Mime::Type.register 'application/json', :geojson
+Mime::Type.register_alias 'application/json', :geojson
 
