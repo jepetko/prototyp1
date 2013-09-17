4ac0daf6af5ece96f88661e3efeb508f2edc385d
katarina <golbang.k@gmail.com>
2013-09-17 11:58:44 +0200

    its already auto saved

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -13,8 +13,6 @@ class Customer < ActiveRecord::Base
   validates :latlon, :presence => true
 
   has_one :company_avatar, :dependent => :destroy, :autosave => true
-  accepts_nested_attributes_for :company_avatar
-
   has_many :contacts, :dependent => :destroy, :autosave => true
 
 end
