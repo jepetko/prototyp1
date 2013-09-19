65dc27da9c232a581bad9dac1cb3de813da48330
katarina <golbang.k@gmail.com>
2013-09-19 12:45:48 +0200

    fixing animation persistance by using fill-mode

--- app/assets/javascripts/spatial/expanders_controller.js
+++ app/assets/javascripts/spatial/expanders_controller.js
@@ -3,11 +3,13 @@
 expandersApp.controller('ExpandersCtrl', ['$scope', 'sharedService', function($scope, sharedService) {
     $scope.state = 'collapsed';
     $scope.btnClicked = function() {
+
+        $('#map-tools').removeClass($scope.state);
         if($scope.state == 'collapsed') {
             $scope.state = 'expanded';
         } else {
             $scope.state = 'collapsed';
         }
-        $('#map-tools').addClass('expanded');
+        $('#map-tools').addClass($scope.state);
     };
 }]);
